de62594ec97b002fbbca7691fd35df60032eb7ea
Shashank Shekhar <shashankuta@gmail.com>
2026-03-22 16:52:55 -0700

    LeetCode 560 | Subarray Sum Equals K | O(n)

--- src/main/java/org/leetcode/SubArraySumEqualsK.java
+++ src/main/java/org/leetcode/SubArraySumEqualsK.java
@@ -1,7 +1,7 @@
 package org.leetcode;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * LeetCode 560 : Subarray Sum Equals K
@@ -19,33 +19,36 @@ public class SubArraySumEqualsK {
      * The key insight is that if prefixSum[j] - prefixSum[i] = k, then the subarray from index i+1 to j has sum k.
      * This transforms the problem: for each position, we want to count how many earlier positions have a prefix sum equal to currentPrefixSum - k.
      * A hash map lets us track prefix sum frequencies as we iterate.
-     * */
+     *
+     */
 
     private static int totalNumberOfSubArrays(int[] numbers, int k) {
         int totalNumberOfSubArrays = 0;
-        int currentPrefixSum = 0;
 
-        Map<Integer, Integer> prefixSumCount = new HashMap<>();
+        int[] prefixSumArray = new int[numbers.length];
 
-        // handles subarrays starting at index 0
-        // we have found one subarray of 0 sum
-        prefixSumCount.put(0, 1);
+        prefixSumArray[0] = numbers[0];
 
-        for (int number : numbers) {
-            // Step 1: update running sum
-            currentPrefixSum = currentPrefixSum + number;
-            System.out.println("currentPrefixSum = " + currentPrefixSum);
+        for (int i = 1; i < numbers.length; i++) {
+            prefixSumArray[i] = prefixSumArray[i - 1] + numbers[i];
+        }
 
-            // Step 2: check if (currentSum - k) exists
-            if(prefixSumCount.containsKey(currentPrefixSum - k)) {
-                totalNumberOfSubArrays += prefixSumCount.get(currentPrefixSum - k);
+        System.out.println("prefixSum = " + Arrays.toString(prefixSumArray));
+        HashMap<Integer, Integer> prefixSumCountMap = new HashMap<>();
+
+        for (int sum : prefixSumArray) {
+            // Case when from index 0 till ith position, we found a subarray whose sum was K
+            if (sum == k) {
+                totalNumberOfSubArrays++;
             }
 
-            // Step 3: store current prefix sum
-            prefixSumCount.put(currentPrefixSum, prefixSumCount.getOrDefault(currentPrefixSum, 0) + 1);
+            if (prefixSumCountMap.containsKey(sum - k)) {
+                totalNumberOfSubArrays = totalNumberOfSubArrays + prefixSumCountMap.get(sum - k);
+            }
 
-            System.out.println("PrefixSumCount Map = " + prefixSumCount.toString());
+            prefixSumCountMap.put(sum, prefixSumCountMap.getOrDefault(sum, 0) + 1);
         }
+
         return totalNumberOfSubArrays;
     }
 }
