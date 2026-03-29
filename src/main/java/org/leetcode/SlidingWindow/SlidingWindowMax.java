468f482df0dbb6d7db1f4cb511a0bb724aedcba1
Shashank Shekhar <shashankuta@gmail.com>
2026-03-29 10:53:43 -0700

    LeetCode 239 | Sliding Window Maximum

--- src/main/java/org/leetcode/SlidingWindow/SlidingWindowMax.java
+++ src/main/java/org/leetcode/SlidingWindow/SlidingWindowMax.java
@@ -1,57 +1,46 @@
 package org.leetcode.SlidingWindow;
 
-import java.util.Arrays;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class SlidingWindowMax {
 
-
     public static void main(String[] args) {
 
-//        int[] numbers = {1, 3, -1, -3, 5, 3, 6, 7};
-        int[] numbers = {9,10,9,-7,-4,-8,2,-6};
-        int k = 5;
+        int[] numbers = {9, 10, 9, -7, -4, -8, 2, -6};
+        int k = 3;
 
         int[] result = slidingWindowWithMaxNumber(numbers, k);
-
         System.out.println("Sliding Window Of Maximum Numbers : " + Arrays.toString(result));
     }
 
     private static int[] slidingWindowWithMaxNumber(int[] numbers, int k) {
 
-
-        if (numbers.length == 1 || k == 1) {
-            return numbers;
-        }
-
         int[] maxNumbersArray = new int[numbers.length - k + 1];
 
-        // custom comparator to create a max-heap
-        // pq.peek() 👉 index of maximum element in current window because of max heap
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (numbers[b] - numbers[a]));
+        Deque<Integer> deQueue = new ArrayDeque<>();
 
+        // 9, 10, 9, -7, -4, -8, 2, -6
         for (int i = 0; i < numbers.length; i++) {
-            // Add current index
-            // Insert into priority queue: O(log n)
-            pq.add(i);
 
-            // Remove indices that are outside the current window
-            // Current window range is: [i - k + 1  →  i]
-            //
-            // Any index <= (i - k) is outside the window (i.e., left of window start)
-            // Example:
-            // i = 5, k = 3 → window = [3, 4, 5]
-            // So index <= 2 must be removed
-            while (!pq.isEmpty() && pq.peek() < i - k + 1) {
-                pq.poll();
+            // Remove Elements from the front of the DeQueue
+            // left/start of window = i - k + 1
+            // right/end of window = i
+            // If the index at the front is too old and no longer inside the current window, remove it.
+            while (!deQueue.isEmpty() && deQueue.peekFirst() <= i - k) {
+                deQueue.pollFirst();
             }
 
-            // Start recording answer once first full window is formed
+            // If the new number is bigger than the numbers at the back, remove those smaller ones.
+            while (!deQueue.isEmpty() && numbers[deQueue.peekLast()] < numbers[i]) {
+                deQueue.pollLast();
+            }
+
+            deQueue.offerLast(i);
+
             if (i >= k - 1) {
-                maxNumbersArray[i - k + 1] = numbers[pq.peek()];
+                maxNumbersArray[i - k + 1] = numbers[deQueue.peekFirst()];
             }
         }
-
         return maxNumbersArray;
     }
 }
