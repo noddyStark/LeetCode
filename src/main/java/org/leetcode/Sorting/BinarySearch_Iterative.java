03073c274c0cb6b89d243b5efe3671915a592e09
Shashank Shekhar <shashankuta@gmail.com>
2026-04-04 15:48:58 -0700

    Binary Search

---
+++ src/main/java/org/leetcode/Sorting/BinarySearch_Iterative.java
@@ -0,0 +1,44 @@
+package org.leetcode.Sorting;
+
+/**
+ * Binary Search only operates on Sorted Arrays and can provide the results in O(logN) time.
+ * It can be implemented in the following two ways
+ * - Iterative Binary Search Algorithm
+ * - Recursive Binary Search Algorithm
+ */
+public class BinarySearch_Iterative {
+
+    public static void main(String[] args) {
+        int[] sortedArray = {2, 4, 6, 8, 10, 12, 13, 15, 16, 19, 23, 27};
+        int numberToBeFound = 15;
+        int result = binarySearch(sortedArray, numberToBeFound);
+
+        if (result == -1) {
+            System.out.println("Element not present in the array!!");
+        } else {
+            System.out.println("Number Present in the Array at index = " + result);
+        }
+    }
+
+    private static int binarySearch(int[] array, int key) {
+        int low = 0;
+        int high = array.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            System.out.println("Mid = " + mid + " low = " + low + " high = " + high);
+
+            if (array[mid] == key) {
+                return mid;
+            }
+
+            if (array[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
